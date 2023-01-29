960506defd0a1145f0bf7cbf701de98d680b5d5d
Ayush Tiwari <ayushtiwari5747462@gmail.com>
2023-01-29 16:54:33 +0530

    Removed backend workspace

--- packages/backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (_req, res) => {
-  res.send('Hello world');
-});
-
-app.listen(1234, () => console.log('Dummy app is listening on port 3000.'));
