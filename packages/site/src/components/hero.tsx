5afeffcc9de2972fcd24fb951f9d21c0d990e431
palaksharma12 <77122363+palaksharma12@users.noreply.github.com>
2023-02-04 18:59:19 +0530

    Changed Landing page
    * Changed Hero
    
    * resolved merge conflicts

--- packages/site/src/components/hero.tsx
+++ packages/site/src/components/hero.tsx
@@ -17,12 +17,12 @@ const items = [
   },
   {
     key: '2',
-    title1: 'Learn and Work better together.',
+    title1: 'Doorway to ETH Based crypto.',
     content: 'MetaMask is the first wallet to offer extensibility. Snaps will boost the rate of innovation not just in Ethereum, but the entire Web3 ecosystem. Now Web3 developers can use the full power of MetaMask and create entirely new types of dapps',
   },
   {
     key: '3',
-    title1: 'Increase your productivity.',
+    title1: 'Doorway to ETH Based crypto.',
     content: 'Another interesting highlight about the Metamask wallet is that it simplifies transactions. You can enter the recipient address and the amount you want to send alongside a transaction fee and click on “Send” to send transactions.',
   },
 ]
@@ -94,22 +94,24 @@ function AppHero() {
   };
 
   return (
-    <div id="hero" className="heroBlock">
+    <div id="hero" className="heroBlock ">
       <Carousel>
         {items.map(item => {
           return (
-            <div key={item.key} className="container-fluid">
+            <div key={item.key} className="container-fluid ">
               <div className="content">
-                <h3>{item.title1}</h3>
-                
+                <div className='hero-content'>
+                <h3>{item.title1}</h3>                
                 <p>{item.content}</p>
                 <div className="btnHolder">
+                  
                   <Button type="primary" size="large" onClick={handleConnectClick}>Connect</Button>
                   <Button type="primary" size="large" href="/insights" target="_blank">View Transaction</Button>
                   {/* <Button size="large"><i className="fas fa-desktop"></i> Watch a Demo</Button> */}
                 </div>
               </div>
-            </div>  
+              </div>
+            </div>
           );
         })}
       </Carousel>
